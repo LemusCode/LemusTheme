d06dcc4a3449d8765bb141ec13f62525b6fca17d
LemusCode <31278486+LemusCode@users.noreply.github.com>
2018-01-17 12:59:37 -0600

    Add files via upload

---
+++ template-parts/content-noauthor.php
@@ -0,0 +1,9 @@
+<div class="loop_pages">
+  <div class="page_name">
+    <i class="fa fa-close"></i> Este author no ha publicado aun
+    <br>
+		Te invitamos a leer mas contenido interesante <i class="fa fa-arrow-right"></i>
+	<br>
+		<i class="fa fa-arrow-down"></i>
+  </div>
+</div>
---
+++ template-parts/content-nopostblog.php
@@ -0,0 +1,9 @@
+<div class="loop_pages">
+  <div class="page_name">
+    <i class="fa fa-close"></i> Este blog aun no tiene posts
+    <br>
+		Te invitamos a leer mas contenido interesante <i class="fa fa-arrow-right"></i>
+	<br>
+		<i class="fa fa-arrow-down"></i>
+  </div>
+</div>
